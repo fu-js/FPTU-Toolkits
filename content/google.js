5d9deb2d305dbd054066fab6ff4d96f1d1462a3a
DAN3002 <dinhanh300229@gmail.com>
2023-10-17 04:34:25 +0700

    feat: Get saved email when login with google

--- content/google.js
+++ content/google.js
@@ -1,7 +1,10 @@
-const STUDENT_EMAIL = 'anhndhe172885@fpt.edu.vn';
-
-const emailEl = document.querySelector('.wLBAL[data-email="' + STUDENT_EMAIL + '"]');
-if (emailEl) {
-	// click on email
-	emailEl.click();
+const main = async () => {
+	const email = await getFromStorage('STUDENT_EMAIL', '');
+	const emailEl = document.querySelector('.wLBAL[data-email="' + email + '"]');
+	
+	if (emailEl) {
+		emailEl.click();
+	}
 }
+
+main();
