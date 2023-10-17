35ea7cb277f0d2cf5ca901a4944eaed59a6fb8b7
DAN3002 <dinhanh300229@gmail.com>
2023-10-17 17:33:27 +0700

    feat: hide auto login option due to problem of world = main in content script

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,14 @@
 const settingForm = document.querySelector('form');
 const emailInput = document.querySelector('#email');
-const autoLoginCheckbox = document.querySelector('#auto-login');
+// const autoLoginCheckbox = document.querySelector('#auto-login');
 
 settingForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const email = e.target.email.value;
-	const autoLogin = e.target.autoLogin.checked;
+	// const autoLogin = e.target.autoLogin.checked;
 
 	setToStorage('STUDENT_EMAIL', email);
-	setToStorage('AUTO_LOGIN', autoLogin);
+	// setToStorage('AUTO_LOGIN', autoLogin);
 
 	alert('Configured');
 
@@ -21,6 +21,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 	const email = await getFromStorage('STUDENT_EMAIL', '');
 	emailInput.value = email;
 
-	const autoLogin = await getFromStorage('AUTO_LOGIN', false);
-	autoLoginCheckbox.checked = autoLogin;
+	// const autoLogin = await getFromStorage('AUTO_LOGIN', false);
+	// autoLoginCheckbox.checked = autoLogin;
 });
